3007b3e2836b01cabc47948426f25acc5970b99a
Arun Prakash M <amariyap@Aruns-MacBook-Pro-2.local>
2024-03-15 11:43:37 +0530

    Include all files required to create pip whl

--- semantic_sam/utils/misc.py
+++ semantic_sam/utils/misc.py
@@ -20,16 +20,18 @@ import torch.distributed as dist
 import torchvision
 from torch import Tensor
 
-from utils.constants import *
+from .constants import *
+
 
 def get_iou(gt_masks, pred_masks, ignore_label=-1):
     rev_ignore_mask = ~(gt_masks == ignore_label)
     gt_masks = gt_masks.bool()
-    n,h,w = gt_masks.shape
-    intersection = ((gt_masks & pred_masks) & rev_ignore_mask).reshape(n,h*w).sum(dim=-1)
-    union = ((gt_masks | pred_masks) & rev_ignore_mask).reshape(n,h*w).sum(dim=-1)
-    ious = (intersection / union)
+    n, h, w = gt_masks.shape
+    intersection = ((gt_masks & pred_masks) & rev_ignore_mask).reshape(n, h * w).sum(dim=-1)
+    union = ((gt_masks | pred_masks) & rev_ignore_mask).reshape(n, h * w).sum(dim=-1)
+    ious = intersection / union
     return ious
+
 
 def _max_by_axis(the_list):
     # type: (List[List[int]]) -> List[int]
@@ -38,6 +40,7 @@ def _max_by_axis(the_list):
         for index, item in enumerate(sublist):
             maxes[index] = max(maxes[index], item)
     return maxes
+
 
 class NestedTensor(object):
     def __init__(self, tensors, mask: Optional[Tensor]):
@@ -60,6 +63,7 @@ class NestedTensor(object):
 
     def __repr__(self):
         return str(self.tensors)
+
 
 def nested_tensor_from_tensor_list(tensor_list: List[Tensor]):
     # TODO make this more general
@@ -103,20 +107,19 @@ def nested_tensor_from_tensor_list(tensor_list: List[Tensor]):
         raise ValueError("not supported")
     return NestedTensor(tensor, mask)
 
+
 def _collate_and_pad_divisibility(tensor_list: list, div=32):
     max_size = []
     for i in range(tensor_list[0].dim()):
-        max_size_i = torch.max(
-            torch.tensor([img.shape[i] for img in tensor_list]).to(torch.float32)
-        ).to(torch.int64)
+        max_size_i = torch.max(torch.tensor([img.shape[i] for img in tensor_list]).to(torch.float32)).to(torch.int64)
         max_size.append(max_size_i)
     max_size = tuple(max_size)
 
-    c,h,w = max_size
+    c, h, w = max_size
     pad_h = (div - h % div) if h % div != 0 else 0
     pad_w = (div - w % div) if w % div != 0 else 0
-    max_size = (c,h+pad_h,w+pad_w)
-    
+    max_size = (c, h + pad_h, w + pad_w)
+
     # work around for
     # pad_img[: img.shape[0], : img.shape[1], : img.shape[2]].copy_(img)
     # m[: img.shape[1], :img.shape[2]] = False
@@ -131,8 +134,9 @@ def _collate_and_pad_divisibility(tensor_list: list, div=32):
         m = torch.zeros_like(img[0], dtype=torch.int, device=img.device)
         padded_mask = torch.nn.functional.pad(m, (0, padding[2], 0, padding[1]), "constant", 1)
         padded_masks.append(padded_mask.to(torch.bool))
-    
+
     return padded_imgs
+
 
 # _onnx_nested_tensor_from_tensor_list() is an implementation of
 # nested_tensor_from_tensor_list() that is supported by ONNX tracing.
@@ -140,9 +144,7 @@ def _collate_and_pad_divisibility(tensor_list: list, div=32):
 def _onnx_nested_tensor_from_tensor_list(tensor_list: List[Tensor]) -> NestedTensor:
     max_size = []
     for i in range(tensor_list[0].dim()):
-        max_size_i = torch.max(
-            torch.stack([img.shape[i] for img in tensor_list]).to(torch.float32)
-        ).to(torch.int64)
+        max_size_i = torch.max(torch.stack([img.shape[i] for img in tensor_list]).to(torch.float32)).to(torch.int64)
         max_size.append(max_size_i)
     max_size = tuple(max_size)
 
@@ -166,6 +168,7 @@ def _onnx_nested_tensor_from_tensor_list(tensor_list: List[Tensor]) -> NestedTen
 
     return NestedTensor(tensor, mask=mask)
 
+
 def is_dist_avail_and_initialized():
     if not dist.is_available():
         return False
@@ -177,61 +180,61 @@ def is_dist_avail_and_initialized():
 def get_class_names(name, background=True):
     if name is None:
         return None
-    if 'refcoco' in name:
+    if "refcoco" in name:
         class_names = ["noun"]
-    elif 'pascal' in name:
+    elif "pascal" in name:
         class_names = PASCAL_CLASSES_PART + ["background"]
-    elif 'sam' in name:
-        class_names = ['foreground'] + ["background"]
-    elif 'coco' in name and 'pan' not in name:
+    elif "sam" in name:
+        class_names = ["foreground"] + ["background"]
+    elif "coco" in name and "pan" not in name:
         class_names = COCO_INSTANCE_CLASSES + ["background"]
-    elif 'coco' in name:
+    elif "coco" in name:
         class_names = COCO_PANOPTIC_CLASSES + ["background"]
-    elif 'ade20k_full' in name:
+    elif "ade20k_full" in name:
         class_names = ADE20K_847 + ["background"]
-    elif 'ade' in name:
+    elif "ade" in name:
         class_names = ADE_PANOPTIC_CLASSES + ["background"]
-    elif 'voc' in name:
+    elif "voc" in name:
         class_names = PASCAL_CLASSES + ["background"]
-    elif 'vlp' in name:
+    elif "vlp" in name:
         class_names = ["noun"]
-    elif 'tsv' in name:
+    elif "tsv" in name:
         class_names = ["noun"]
-    elif 'phrasecut' in name:
+    elif "phrasecut" in name:
         class_names = ["noun"]
-    elif 'openimage' in name:
+    elif "openimage" in name:
         class_names = ["noun"]
-    elif 'imagenet' in name:
+    elif "imagenet" in name:
         class_names = IMAGENET_CLASSES
-    elif 'context_459' in name:
+    elif "context_459" in name:
         class_names = PASCAL_CONTEXT_459 + ["background"]
-    elif 'context_59' in name:
+    elif "context_59" in name:
         class_names = PASCAL_CONTEXT_59 + ["background"]
-    elif 'context_33' in name:
+    elif "context_33" in name:
         class_names = PASCAL_CONTEXT_33
-    elif 'sunrgbd_37' in name:
+    elif "sunrgbd_37" in name:
         class_names = SUN_RGBD_37
-    elif 'scannet_41' in name:
+    elif "scannet_41" in name:
         class_names = SCAN_40
-    elif 'scannet_38' in name:
+    elif "scannet_38" in name:
         class_names = SCAN_37
-    elif 'scannet_21' in name:
+    elif "scannet_21" in name:
         class_names = SCAN_20
-    elif 'object365' in name:
+    elif "object365" in name:
         class_names = OBJECT365
-    elif 'lvis' in name:
+    elif "lvis" in name:
         class_names = LVIS_CATEGORIES
-    elif 'seginw' in name:
-        class_names = SEGINW_CATEGORIES[name.replace('_train', '').replace('_val', '')] + ["background"]
-    elif name == 'cityscapes_fine_sem_seg_val':
+    elif "seginw" in name:
+        class_names = SEGINW_CATEGORIES[name.replace("_train", "").replace("_val", "")] + ["background"]
+    elif name == "cityscapes_fine_sem_seg_val":
         class_names = CITYSCAPES
-    elif name == 'cityscapes_fine_instance_seg_val':
+    elif name == "cityscapes_fine_instance_seg_val":
         class_names = CITYSCAPES_THING + ["background"]
-    elif name in ['cityscapes_fine_panoptic_val', 'cityscapes_fine_panoptic_train']:
+    elif name in ["cityscapes_fine_panoptic_val", "cityscapes_fine_panoptic_train"]:
         class_names = CITYSCAPES + ["background"]
-    elif name == 'bdd10k_val_sem_seg':
+    elif name == "bdd10k_val_sem_seg":
         class_names = BDD_SEM
-    elif name == 'bdd10k_40_panoptic_val':
+    elif name == "bdd10k_40_panoptic_val":
         class_names = BDD_PANO
     else:
         assert False, "text dataset name {} is not defined".format(name)
@@ -241,7 +244,8 @@ def get_class_names(name, background=True):
 
     return class_names
 
-# TODO: add background to 
+
+# TODO: add background to
 # def get_class_names(name):
 #     if name is None:
 #         return None
